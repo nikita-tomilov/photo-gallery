4e28717d570856aa2aa6cea083ee7d554e805bf8
Nikita Tomilov <programmer174@icloud.com>
2022-07-29 07:47:34 +0200

    all: ensure that existing entities in the DB exist in the FS

--- src/main/kotlin/com/nikitatomilov/photogallery/service/MediaLibraryParser.kt
+++ src/main/kotlin/com/nikitatomilov/photogallery/service/MediaLibraryParser.kt
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service
 import java.io.File
 import java.time.Instant
 import java.time.ZoneOffset
-import java.util.concurrent.Executors
 import javax.annotation.PostConstruct
 
 @Service
@@ -21,20 +20,19 @@ class MediaLibraryParser(
   @Autowired private val mediaEntityRepository: MediaEntityRepository
 ) {
 
-  //private val ex = Executors.newFixedThreadPool(THREADS)
-
   private lateinit var rootDirs: List<File>
 
   @PostConstruct
-  fun postConstruct() {
-    logger.info { "Launched with the following dirs: ${rootPaths.toList()}" }
+  fun reindexDatabase() {
+    logger.info { "Launched reindexing with the following dirs: ${rootPaths.toList()}" }
     rootDirs = rootPaths.map { path ->
       File(path).also { if (!it.isDirectory) error("$it is not a directory") }
     }
+    ensureAlreadyExistingFiles()
     parse()
   }
 
-  fun parse(): Map<Int, List<FilesystemMediaEntity>> {
+  fun parse(): Map<Int, List<MediaEntity>> {
     return parse(rootDirs.associateWith { getMediaFiles(it) })
   }
 
@@ -46,49 +44,35 @@ class MediaLibraryParser(
     return allFiles.filter { it.isPhoto() || it.isVideo() }
   }
 
-  private fun parse(files: Map<File, List<File>>): Map<Int, List<FilesystemMediaEntity>> {
+  private fun parse(files: Map<File, List<File>>): Map<Int, List<MediaEntity>> {
     val mediaFiles =
         files.map { (fromDir, files) -> files.map { FilesystemMediaEntity(fromDir, it) } }.flatten()
     logger.warn { "Found ${mediaFiles.size} media files. Parsing metadata..." }
-    mediaFiles.forEachIndexed { i, it -> index(it, i + 1, mediaFiles.size) }
-//    val mediaFilesBatches = mediaFiles.shuffled().batched(THREADS)
-//    val latch = CountDownLatch(mediaFiles.size)
-//    mediaFilesBatches.forEach { mediaFilesBatch ->
-//      ex.submit {
-//        mediaFilesBatch.forEach {
-//          parseAndConvert(it)
-//          latch.countDown()
-//        }
-//      }
-//    }
-//    var finished = false
-//    var prev = latch.count
-//    var prevMs = System.currentTimeMillis()
-//    while (!finished) {
-//      val ok = latch.await(1, TimeUnit.SECONDS)
-//      if (ok) finished = ok
-//      val cur = latch.count
-//      if (prev - cur > 1000) {
-//        val curMs = System.currentTimeMillis()
-//        val ms = curMs - prevMs
-//        logger.warn { "$cur files left to parse out of total ${mediaFiles.size}; $ms spent for ${prev - cur} files" }
-//        prev = cur
-//        prevMs = curMs
-//      }
-//    }
-//    println("Done")
+    val entities = mediaFiles
+        .mapIndexed { i, it -> index(it, i + 1, mediaFiles.size) }
+        .filterNotNull()
 
-    val correctDateFound = mediaFiles.filter { it.correctDateFound() }
-    val correctDateNotFound = mediaFiles.filterNot { it.correctDateFound() }
-
-    logger.warn { "Failed to find date for ${correctDateNotFound.size} files:" }
-    correctDateNotFound.forEach { logger.warn { " - $it" } }
-
-    val byYear = correctDateFound.groupBy {
-      val i = Instant.ofEpochMilli(it.date).atOffset(ZoneOffset.UTC)
+    val byYear = entities.groupBy {
+      val i = Instant.ofEpochMilli(it.parsedDate).atOffset(ZoneOffset.UTC)
       i.year
     }
     return byYear
+  }
+
+  private fun ensureAlreadyExistingFiles() {
+    val existing = mediaEntityRepository.findAll()
+    logger.warn { "${existing.size} entities exist in the DB" }
+    val nonExistingAnymore = ArrayList<MediaEntity>()
+    existing.forEach {
+      if (!File(it.fullPath).exists()) {
+        nonExistingAnymore.add(it)
+      }
+    }
+    if (nonExistingAnymore.isNotEmpty()) {
+      logger.warn { "${nonExistingAnymore.size} entities were not found, removing from DB" }
+      nonExistingAnymore.forEach { logger.warn { " - ${it.fullPath}" } }
+      mediaEntityRepository.deleteAll(nonExistingAnymore)
+    }
   }
 
   private fun index(fsEntity: FilesystemMediaEntity, i: Int, n: Int): MediaEntity? {
@@ -102,7 +86,6 @@ class MediaLibraryParser(
       if (existingByPath.size == 1) {
         val found = existingByPath.single()
         if (found.fullPath == fsEntity.file.absolutePath) {
-          logger.info { "[$i/$n] ${fsEntity.file.absolutePath} already exists with id ${found.id}" }
           return found
         }
       }
